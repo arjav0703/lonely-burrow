e6ad251c5043f10d13c94a9326441eda0280ea64
arjav0703 <arjavjain0703@gmail.com>
2025-05-11 11:36:14 +0530

    update boot screen

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -17,11 +17,33 @@
       mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
       magicOrExtension = ''\x7fELF....AI\x02'';
     };
-    plymouth.enable = true;
-    plymouth.logo = "/home/arjav/Pictures/wallpapers/Wall.png";
-    kernelParams = [ "quiet" "splash" ];
-    consoleLogLevel = 3;
-    initrd.verbose = false;
-    initrd.systemd.enable = true;
+
+    # THEME for bootloader
+    plymouth = {
+          enable = true;
+          theme = "rings";
+          themePackages = with pkgs; [
+            # By default we would install all themes
+            (adi1090x-plymouth-themes.override {
+              selected_themes = [ "rings" ];
+            })
+          ];
+        };
+
+        # Enable "Silent boot"
+        consoleLogLevel = 3;
+        initrd.verbose = false;
+        kernelParams = [
+          "quiet"
+          "splash"
+          "boot.shell_on_fail"
+          "udev.log_priority=3"
+          "rd.systemd.show_status=auto"
+        ];
+        # Hide the OS choice for bootloaders.
+        # It's still possible to open the bootloader list by pressing any key
+        # It will just not appear on screen unless a key is pressed
+        loader.timeout = 0;
+
   };
 }
